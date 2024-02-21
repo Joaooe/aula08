97dbe76fabf1832d772cb598285c4e7423d803b1
Joaooe <149209354+Joaooe@users.noreply.github.com>
2024-02-20 21:53:28 -0300

    Add files via upload

---
+++ boom.js
@@ -0,0 +1,11 @@
+for (var boom = 10; boom >= 0; boom--) {
+    if (boom === 0) {
+        console.log("boom");
+    }
+    else if (boom % 2 === 0) {
+        console.log("tic");
+    }
+    else {
+        console.log("tac");
+    }
+}
